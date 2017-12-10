68c0d4e6b109a6bbfce217ea6d923f646c40bca7
Sven Adolph <mail@svenadolph.net>
2017-12-10 16:17:23 +0100

    day 3 part2

--- src/day3/Day3.kt
+++ src/day3/Day3.kt
@@ -8,13 +8,11 @@ fun main(args: Array<String>) {
     println("All: " + calcAll(12))
     println("All: " + calcAll(23))
     println("All: " + calcAll(45))
-    println("All: " + calcAll(1024))
-    println("All: " + calcAll(289326))*/
+    println("All: " + calcAll(1024))*/
 
-    partTwo(49)
+    println("Part One: " + calcAll(289326))
 
-    //??????
-
+    partTwo(289326)
 
 }
 
@@ -23,52 +21,82 @@ fun partTwo(n: Int) {
     val width = Math.sqrt(n.toDouble())
     println(width)
 
-    val m = 7
+    val m = 5001
     val board = Array(m, { IntArray(m) })
 
-    print2D(board)
+    //print2D(board)
 
     var x = m / 2
     var y = m / 2
 
     board[y][x] = 1
+    x += 1
 
-    x+=1
 
-    calcArroundCoord(x,y, board)
 
-    while (board[y][x] < n){
-        if(board[y][x-1] != 0){
-            y+=1
-        }else if (board[y][x-1] == 0 && board[y-1][x-1] != 0){
-            x-=1
-        }else if(board[y][x-1] == 0 && board[y-1][x] != 0){
-            x-=1
-        }else if(board[y-1][x] == 0 && board[y-1][x+1] != 0){
-            y-=1
-        }else if (board[y-1][x] == 0 && board[y-1][x+1] != 0){
-            y-=1
-        }else if (board[y][x+1] == 0 && board[y+1][x-1] != 0){
-            x+=1
-        }else{
-            x+=1
+    calcArroundCoord(y, x, board)
+
+    //print2D(board)
+
+//    calcArroundCoord(2, 4, board)
+
+
+    while (board[y][x] < n) {
+        if (board[y][x - 1] != 0 && board[y - 1][x] == 0) {
+            y -= 1
+        } else if (board[y][x - 1] == 0 && board[y + 1][x - 1] != 0) {
+            x -= 1
+        } else if (board[y][x - 1] == 0 && board[y + 1][x] != 0) {
+            x -= 1
+        } else if (board[y + 1][x] == 0 && board[y + 1][x + 1] != 0) {
+            y += 1
+        } else if (board[y + 1][x] == 0 && board[y + 1][x + 1] != 0) {
+            y += 1
+        } else if (board[y][x + 1] != 0 && board[y + 1][x] == 0) {
+            y += 1
+        } else if (board[y][x + 1] == 0 && board[y - 1][x - 1] != 0) {
+            x += 1
+        } else {
+            x += 1
         }
-        println("$x $y")
-        print2D(board)
-        calcArroundCoord(x,y,board)
-    }
 
-    print2D(board)
+        //print2D(board)
+        calcArroundCoord(y, x, board)
+
+    }
+    println("PART TWO: $y-$x: " + board[y][x])
+
+//    print2D(board)
+
+//    print2DSimple(board)
 }
 
-fun calcArroundCoord(x: Int, y: Int, arr: Array<IntArray>) {
-    arr[y][x] = arr[y -1][x]+arr[y +1][x]+arr[y][x +1]+arr[y][x -1]
+fun calcArroundCoord(y: Int, x: Int, arr: Array<IntArray>) {
+    arr[y][x] = arr[y][x - 1] +
+            arr[y - 1][x - 1] +
+            arr[y + 1][x - 1] +
+            arr[y - 1][x] +
+            arr[y + 1][x] +
+            arr[y][x + 1] +
+            arr[y - 1][x + 1] +
+            arr[y + 1][x + 1]
 }
 
 fun print2D(arr: Array<IntArray>) {
+    for ((i, line) in arr.withIndex()) {
+        for ((j, element) in line.withIndex()) {
+            print("$i-$j($element)")
+        }
+        println()
+    }
+    println("---")
+}
+
+fun print2DSimple(arr: Array<IntArray>) {
     for (intArr in arr) {
         println(Arrays.toString(intArr))
     }
+
 }
 
 
